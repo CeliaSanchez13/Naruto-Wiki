11af19518706fce48aa71b583ff4a37149b85612
CeliaSullivan <celiasanchez4ac@gmail.com>
2023-09-08 09:46:56 +0200

    version1 menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { ClansComponent } from './pages/clans/clans.component';
+import { VillagesComponent } from './pages/villages/villages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:HomeComponent},
+  { path:'home', component:HomeComponent},
+  { path:'characters', component:CharactersComponent},
+  { path:'clans', component:ClansComponent},
+  { path:'villages', component:VillagesComponent},
+  { path:'**', redirectTo:'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { ClansComponent } from './pages/clans/clans.component';
+import { VillagesComponent } from './pages/villages/villages.component';
+import { AkatsukiComponent } from './pages/akatsuki/akatsuki.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    CharactersComponent,
+    ClansComponent,
+    VillagesComponent,
+    AkatsukiComponent
   ],
   imports: [
     BrowserModule,
