0b07d462943a58f82ed2d747f3d779411738a0ad
CeliaSullivan <celiasanchez4ac@gmail.com>
2023-09-12 10:11:19 +0200

    limpieza de codigo

--- src/app/shared/table-list/table-list.component.ts
+++ src/app/shared/table-list/table-list.component.ts
@@ -12,81 +12,59 @@ import { ServicesService } from 'src/app/services/services.service';
 export class TableListComponent implements OnInit{
 
   carga:boolean = false;
-  acceso:boolean = false;
-
-  pagTotal:number = 1 ;
   paginasTotales:number = 0;
-
   page:number = 1;
   pageSize:number = 20;
   totalCharacters:number = 0;
 
   array:Character[] = [];
 
-  pagination:Character[] = [];
-
   constructor(private _serviceService: ServicesService){
-  this.verArray();
+
   }
 
   ngOnInit(): void {
     
-    this.getCharactersListInfo(); 
     this.getCharactersListForPagination(); 
-    this.verArray();
+   
   }
 
-  pagina(page:any){
-    console.log(page);
-  }
+  //METODOS
 
   getCharactersListForPagination(){
 
     if ( !this.carga ){
-      this._serviceService.allCharactersList(this.pagTotal).subscribe(
+      this._serviceService.allCharactersList(1).subscribe(
         (resp:any) => {
+          this.page = resp.currentPage;
+          this.pageSize = resp.pageSize;
+          this.totalCharacters = resp.totalCharacters;
+      
+          this.paginasTotales = Math.round(this.totalCharacters / this.pageSize );
+          console.log("La longitud es "+ this.paginasTotales);
+      
           this.array = resp.characters;
           this.carga = true;
-          this.pagTotal++
           this.getCharactersListForPagination();
         }
       );
-    }
+    }//EndIF
+
     if ( this.carga ){
       for ( let i = 2 ;i<this.paginasTotales;i++ ){
         for( let j = 0; j < this.pageSize; j++){
           this._serviceService.allCharactersList(i).subscribe(
             (resp:any) => {
-              console.log(resp.characters[j]);
               this.array.push(resp.characters[j]);
-              //console.log('Longitud del array'+ this.array.length+ ' en la pagina '+ i + ' objeto'+ j +'con el ID '+resp.characters[j].id + ' y el nombre '+ resp.characters[j].name);
             }
           )
         }
       }
-    }
+    }//EndIF
 
-  }
+  }//End getCharactersListForPagination()
 
-  getCharactersListInfo(){
 
-    this._serviceService.allCharactersList(1).subscribe(
-      (resp:any) => {
-        this.page = resp.currentPage;
-        this.pageSize = resp.pageSize;
-        this.totalCharacters = resp.totalCharacters;
-      
-        this.paginasTotales = Math.round(this.totalCharacters / this.pageSize );
-        console.log("La longitud es "+ this.paginasTotales);
-      }
-    );
-  }
-
-  verArray(){
-    
-      console.log(this.array);
-    
-  }
 
 }
 
