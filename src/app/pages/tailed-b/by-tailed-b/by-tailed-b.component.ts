c69b13b9a2e8332f116a0c56139a59e37837c844
CeliaSullivan <celiasanchez4ac@gmail.com>
2023-09-18 11:44:54 +0200

    pagina bijus v1

--- src/app/pages/tailed-b/by-tailed-b/by-tailed-b.component.ts
+++ src/app/pages/tailed-b/by-tailed-b/by-tailed-b.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TailedBeast } from 'src/app/interfaces/tailedB.interface';
+import { ServicesService } from 'src/app/services/services.service';
 
 @Component({
   selector: 'app-by-tailed-b',
   templateUrl: './by-tailed-b.component.html',
   styleUrls: ['./by-tailed-b.component.css']
 })
-export class ByTailedBComponent {
+export class ByTailedBComponent implements OnInit{
 
+  index:number=0;
+  beasts?:TailedBeast;
+
+  constructor( private _servicioService:ServicesService,
+               private route: ActivatedRoute){}
+
+
+  ngOnInit(): void {
+    this.index = this.route.snapshot.params['id'];
+    this.recuperarCharacter();
+  }
+
+  //Metodos
+  recuperarCharacter(){
+    this._servicioService.getTailedB(this.index).subscribe(
+      (resp:any) => {
+        this.beasts = resp;
+        console.log(this.beasts)
+      }
+    )
+  }
 }
