aa933157ce51683ec19bac01a40dff368e29f58a
CeliaSullivan <celiasanchez4ac@gmail.com>
2023-10-02 11:05:39 +0200

    login aparentemente hecho

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user.interface';
 import { BlogService } from 'src/app/services/blog.service';
 import { ValidatorService } from 'src/app/shared/service/validator.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +18,8 @@ export class LoginComponent implements OnInit{
 
   constructor( private _blogService:BlogService,
                private _validatorService:ValidatorService,
-               private fb:FormBuilder){}
+               private fb:FormBuilder,
+               private route:Router){}
 
   ngOnInit(): void {
     
@@ -41,9 +44,28 @@ export class LoginComponent implements OnInit{
     console.log(this.formu.value);
     for( let i=0 ; i <= this.usuarios.length ; i++){
       if( this.usuarios[i]?.email === this.formu.controls['email'].value ){
-        console.log('si existe el email')
-      }
-      
-    };
-  }
+        //El email existe
+        if ( this.usuarios[i]?.password === this.formu.controls['password'].value){
+          console.log("USUARIO correcto!")
+          Swal.fire({
+            icon: 'success',
+            title: `Welcome ${ this.usuarios[i]?.user }`,
+            text: 'User found!',
+            showConfirmButton: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+              //Insertamos los datos del usuario en el localStorage
+              localStorage.setItem('email', `${ this.usuarios[i]?.email }`);
+              localStorage.setItem('user', `${ this.usuarios[i]?.user }`);
+              localStorage.setItem('date', `${ this.usuarios[i]?.date }`);
+              // Este será la imagen localStorage.setItem('img', `${ this.usuarios[i]?.img }`);
+              this.route.navigate(['/home']);
+              //window.location.reload(); //Recargar la pagina entera para que podamos ver la actualización en el menú y ver el usuario nuevo.
+            }
+          });//Fin_swal
+        }
+      }   
+    };//Fin_for
+  }//Fin_submit
+
 }
